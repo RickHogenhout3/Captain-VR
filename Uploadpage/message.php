c2ca165d0567e862250b4d942bc92c486bc27a72
Luke de Boer <119858339+Lukedeboerr@users.noreply.github.com>
2023-01-31 15:56:08 +0100

    Add files via upload

--- Uploadpage/message.php
+++ Uploadpage/message.php
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 <?php
 
 session_start();
 
 var_dump($_SESSION['message']);
 ?>
+=======
+<?php
+
+session_start();
+
+var_dump($_SESSION['message']);
+?>
+>>>>>>> 3b31429a04539209dbaf8af5acb697f8b24b37d7
--- Uploadpage/readfile.php
+++ Uploadpage/readfile.php
@@ -1,3 +1,40 @@
+<<<<<<< HEAD
+<?php
+include("config.php");
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $fetchVideos = $conn->query("SELECT * FROM videos ORDER BY id DESC");
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+<!doctype html>
+<html>
+  <head>
+    <title>Upload and Store video to MySQL Database with PHP</title>
+  </head>
+  <body>
+    <div>
+ 
+     <?php
+        while ($row = $fetchVideos->fetch(PDO::FETCH_ASSOC)) {
+            $location = explode('htdocs', $row['location'])[1];
+            $name = $row['name'];
+            echo "<div style='float: left; margin-right: 5px;'>
+          <video src='" . $location . "' controls width='320px' height='320px' ></video>
+          <br>
+          <span>" . $name . "</span>
+       </div>";
+        }
+        ?>
+ 
+    </div>
+
+  </body>
+</html>
+=======
 <?php
 include("config.php");
 try {
@@ -33,3 +70,4 @@ try {
 
   </body>
 </html>
+>>>>>>> 3b31429a04539209dbaf8af5acb697f8b24b37d7
