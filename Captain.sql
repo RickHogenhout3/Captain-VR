d3a255e245d3728d39ec4679e07368fb25e02f41
Rick Hogenhout <88785226+RickHogenhout3@users.noreply.github.com>
2023-02-01 14:47:02 +0100

    Update Captain.sql

--- Captain.sql
+++ Captain.sql
@@ -6,12 +6,14 @@ USE captain_vr;
 
 CREATE TABLE users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    firstname VARCHAR(50) NOT NULL UNIQUE,
+    lastname VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 CREATE TABLE coaches (
     coach_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    firstname VARCHAR(50) NOT NULL UNIQUE,
+    lastname VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(1050) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
@@ -20,5 +22,5 @@ CREATE TABLE videos (
    name VARCHAR(255) NOT NULL,
    location VARCHAR(255) NOT NULL
 );
-INSERT INTO `coaches` (`coach_id`, `username`, `email`, `password`) VALUES ('1', 'admin', 'admin@gmail.com', 'admin123');
-INSERT INTO `users` (`id`, `username`, `password`) VALUES ('1', 'user', 'wordpass12');
+INSERT INTO `coaches` (`coach_id`, `firstname`, `lastname`, `email`, `password`) VALUES ('1', 'admin', 'admin@gmail.com', 'admin123');
+INSERT INTO `users` (`id`, `firstname`,`lastname`, `password`) VALUES ('1', 'user', 'wordpass12');
