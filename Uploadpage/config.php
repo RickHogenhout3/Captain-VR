245c1ba5665b22efa06d5ebe8b250eba8d07cc2c
Rick Hogenhout <88785226+RickHogenhout3@users.noreply.github.com>
2023-02-01 14:56:19 +0100

    Update config.php

--- Uploadpage/config.php
+++ Uploadpage/config.php
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
-<?php
- session_start();
+<?php  
+ session_start();  
  $servername = "localhost";
  $username = "bit_academy";
  $password = "bit_academy";
  $dbname = "captain_vr";
  $charset = "utf8mb4";
-try {
-      $connect = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
+try {  
+      $connect = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);  
       $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);}
-      catch (PDOException $error) {
-        $message = $error->getMessage();
-=======
-<?php
- session_start();
- $servername = "localhost";
- $username = "bit_academy";
- $password = "bit_academy";
- $dbname = "captain_vr";
- $charset = "utf8mb4";
-try {
-      $connect = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
-      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);}
-      catch (PDOException $error) {
-        $message = $error->getMessage();
->>>>>>> 3b31429a04539209dbaf8af5acb697f8b24b37d7
-    }  ?>
+      catch (PDOException $error) {  
+        $message = $error->getMessage();  
+    }
+?>
