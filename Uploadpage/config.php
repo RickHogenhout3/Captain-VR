45399a765ce03791b9ceb845911c87b8ac318afb
Fabian van Unen <171452@student.horizoncollege.nl>
2023-02-02 09:15:04 +0100

    uploadpage v2.2

--- Uploadpage/config.php
+++ Uploadpage/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
  session_start();
  $servername = "localhost";
@@ -11,20 +10,5 @@ try {
       $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);}
       catch (PDOException $error) {
         $message = $error->getMessage();
-    }  ?>
-=======
-<?php  
- session_start();  
- $servername = "localhost";
- $username = "bit_academy";
- $password = "bit_academy";
- $dbname = "captain_vr";
- $charset = "utf8mb4";
-try {  
-      $connect = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);  
-      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);}
-      catch (PDOException $error) {  
-        $message = $error->getMessage();  
-    }
+    }  
 ?>
->>>>>>> 6c336d213541abdd2d845fec8020da9cc54eff1c
--- Uploadpage/upload.php
+++ Uploadpage/upload.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include("config.php");
 
@@ -108,73 +107,3 @@ if (isset($_POST['but_upload'])) {
 </body>
 
 </html>
-=======
-<?php
-include("config.php");
- 
-if (isset($_POST['but_upload'])) {
-    $maxsize = 2000000; // 2MB
-    if (isset($_FILES['file']['name']) && $_FILES['file']['name'] != '') {
-        $name = $_FILES['file']['name'];
-        $target_dir = __DIR__ . '/videos/';
-        $target_file = $target_dir . $_FILES["file"]["name"];
-
-        // Select file type
-        $extension = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-        // Valid file extensions
-        $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
-
-        // Check extension
-        if (in_array($extension, $extensions_arr)) {
-           // Check file size
-            if (($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
-                $_SESSION['message'] = "File too large. File must be less than 5MB.";
-            } else {
-               // Upload
-                if (move_uploaded_file($_FILES['file']['tmp_name'], $target_file)) {
-                  // Insert record
-                    $query = "INSERT INTO videos(name,location) VALUES(:name, :location)";
-
-                    $stmt = $connect->prepare($query);
-                    $stmt->execute([
-                        'name' => $name,
-                        'location' => $target_file
-                    ]);
-                    $_SESSION['message'] = "Upload successfully.";
-                } else {
-                    $_SESSION['message'] = 'upload unsuccess' . $_FILES['file']['tmp_name'];
-                }
-            }
-        } else {
-            $_SESSION['message'] = "Invalid file extension.";
-        }
-    } else {
-        $_SESSION['message'] = "Please select a file.";
-    }
-    header('location: readfile.php');
-    exit;
-} 
-?>
-<!doctype html> 
-<html> 
-  <head>
-     <title>Upload and Store video to MySQL Database with PHP</title>
-  </head>
-  <body>
-
-    <!-- Upload response -->
-    <?php 
-    if (isset($_SESSION['message'])) {
-        echo $_SESSION['message'];
-        unset($_SESSION['message']);
-    }
-    ?>
-    <form method="post" action="" enctype='multipart/form-data'>
-      <input type='file' name='file' />
-      <input type='submit' value='Upload' name='but_upload'>
-    </form>
-
-  </body>
-</html>
->>>>>>> 6c336d213541abdd2d845fec8020da9cc54eff1c
